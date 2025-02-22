598e8ce048e22b978034efed907aea2820b7e913
vinimateuslima <viniciusmateus.dev@gmail.com>
2025-02-22 16:10:03 -0300

    criando funcionalidade de adicionar amigo

---
+++ app.js
@@ -0,0 +1,44 @@
+//O principal objetivo deste desafio é fortalecer suas habilidades em lógica de programação. Aqui você deverá desenvolver a lógica para resolver o problema.
+
+let amigo = document.getElementById("amigo");
+
+let arrayAmigos = [];
+
+document.addEventListener("keypress", (e) => {
+  if (e.code == "Enter") {
+    adicionarAmigo();
+  }
+});
+
+function adicionarAmigo() {
+  if (amigo.value == "") {
+    return alert("Digite um nome válido");
+  }
+
+  atualizarLista(amigo.value);
+
+  amigo.value = "";
+}
+
+function atualizarLista(amigo) {
+  let listaAmigos = document.getElementById("listaAmigos");
+  let li = document.createElement("LI");
+  li.innerHTML = amigo;
+  listaAmigos.appendChild(li);
+  arrayAmigos.push(amigo);
+}
+
+function sortearAmigo() {
+  if (arrayAmigos.length <= 0) {
+    return alert("A lista de amigos está vazia!");
+  }
+
+  let posicao = Math.floor(Math.random() * (arrayAmigos.length - 0) + 0);
+  let amigoSorteado = arrayAmigos[posicao];
+
+  document.getElementById(
+    "resultado"
+  ).innerText = `O amigo secreto sorteado é: ${amigoSorteado}`;
+
+  
+}
